f1b6dcc9a0d2679d10ba166dce243ca16ad356c5
imane <imanesamadi21@gmail.com>
2025-04-07 15:52:43 +0100

    Fixed rendering issues

--- src/components/aboutComponents/WhoWeAre.vue
+++ src/components/aboutComponents/WhoWeAre.vue
@@ -1,28 +1,39 @@
 <script setup>
+import { onMounted, nextTick } from 'vue'
+
 const props = defineProps({
 	whoWeAre: {
 		type: Object,
-		default: () => ({ description: '', link: { text: '', url: '' } }),
+		default: () => ({
+			description: '',
+			link: { text: '', url: '' },
+		}),
+	},
+})
+
+onMounted(async () => {
+	await nextTick()
+	if (window.AOS) {
+		window.AOS.refreshHard()
+		console.log('[AOS] Refreshed on dynamic mount.')
 	}
 })
 </script>
 
-
 <template>
-	<!--===== WHO WE ARE AREA STARTS =======-->
 	<div class="about10-section-area sp1" style="background-color: #000238;">
 		<div class="container">
 			<div class="row align-items-center">
 				<div class="col-lg-6">
 					<div class="about10-images">
 						<div class="img1" data-aos="zoom-in" data-aos-duration="1000">
-							<img src="/assets/img/all-images/about/about-img7.png" alt="" />
+							<img src="/assets/img/all-images/about/about-img7.png" alt="About 1" />
 						</div>
 						<div class="img2" data-aos="zoom-in" data-aos-duration="1100">
-							<img src="/assets/img/all-images/about/about-img8.png" alt="" />
+							<img src="/assets/img/all-images/about/about-img8.png" alt="About 2" />
 						</div>
 						<div class="img3" data-aos="zoom-in" data-aos-duration="1200">
-							<img src="/assets/img/all-images/about/about-img9.png" alt="" />
+							<img src="/assets/img/all-images/about/about-img9.png" alt="About 3" />
 						</div>
 					</div>
 				</div>
@@ -30,23 +41,32 @@ const props = defineProps({
 				<div class="col-lg-6">
 					<div class="about-header heading13">
 						<h3 data-aos="fade-left" data-aos-duration="800">
-							<img src="/assets/img/logo/logo-1.png" alt="" />
+							<img src="/assets/img/logo/logo-1.png" alt="Logo" />
 							Who We Are
 						</h3>
 						<div class="space18"></div>
-						<p data-aos="fade-left" data-aos-duration="900">{{ whoWeAre.description }}</p>
+
+						<p data-aos="fade-left" data-aos-duration="900">
+							{{ whoWeAre.description }}
+						</p>
+
 						<div class="space12"></div>
 						<div class="space32"></div>
-						<div class="btn-area1" data-aos="fade-left" data-aos-duration="1200">
+						<div
+							v-if="whoWeAre.link.text && whoWeAre.link.url"
+							class="btn-area1"
+							data-aos="fade-left"
+							data-aos-duration="1200"
+						>
 							<a :href="whoWeAre.link.url" class="vl-btn10">
 								{{ whoWeAre.link.text }}
-								<img src="/assets/img/icons/arrow2.svg" alt="" />
+								<img src="/assets/img/icons/arrow2.svg" alt="Arrow Icon" />
 							</a>
 						</div>
+
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!--===== WHO WE ARE AREA ENDS =======-->
 </template>
--- src/components/agendaComponents/Agenda.vue
+++ src/components/agendaComponents/Agenda.vue
@@ -1,11 +1,21 @@
 <script setup>
-defineProps({
+import { onMounted, nextTick } from 'vue'
+
+const props = defineProps({
   data: Object
 });
+
+onMounted(async () => {
+  await nextTick();
+  if (window.AOS) {
+    window.AOS.refreshHard();
+    console.log('[AOS] Refreshed on dynamic mount.');
+  }
+})
 </script>
 
 <template>
-  <div class="event8-section-area sp1" style="background-color: #000238;" v-if="data">
+  <div class="event8-section-area sp1" style="background-color: #000238;" v-if="data?.days?.length">
     <div class="container">
       <!-- TABS HEADER -->
       <div class="row">
@@ -14,10 +24,15 @@ defineProps({
             <div class="tabs-button">
               <ul class="nav nav-pills" id="pills-tab" role="tablist">
                 <li v-for="(day, index) in data.days" :key="index" class="nav-item" role="presentation">
-                  <button class="nav-link" :class="{ active: index === 0 }"
-                    :id="`pills-day${index}-tab`" data-bs-toggle="pill"
-                    :data-bs-target="`#pills-day${index}`" type="button" role="tab"
-                    :aria-controls="`pills-day${index}`"
+                  <button 
+                    class="nav-link" 
+                    :class="{ active: index === 0 }" 
+                    :id="`pills-day${index}-tab`"
+                    data-bs-toggle="pill" 
+                    :data-bs-target="`#pills-day${index}`" 
+                    type="button" 
+                    role="tab"
+                    :aria-controls="`pills-day${index}`" 
                     :aria-selected="index === 0 ? 'true' : 'false'">
                     <span class="calender">
                       <img src="/assets/img/icons/calender2.svg" alt="" />
@@ -32,10 +47,12 @@ defineProps({
             </div>
 
             <!-- Download Button -->
-            <div class="btn-area" v-if="data.overview && data.overview.downloadLink">
+            <div class="btn-area" v-if="data.overview?.downloadLink">
               <div class="btn-area1">
                 <a class="vl-btn8" :href="data.overview.downloadLink" download>
-                  <span class="demo">Download Full Agenda <i class="fa-solid fa-download"></i></span>
+                  <span class="demo">
+                    Download Full Agenda <i class="fa-solid fa-download"></i>
+                  </span>
                 </a>
               </div>
             </div>
@@ -47,40 +64,71 @@ defineProps({
       <div class="row">
         <div class="col-lg-12" data-aos="fade-up" data-aos-duration="1000">
           <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade"
-              :class="{ show: index === 0, active: index === 0 }"
-              v-for="(day, index) in data.days"
-              :key="'content-' + index"
+            <div 
+              v-for="(day, index) in data.days" 
+              :key="'content-' + index" 
               :id="`pills-day${index}`"
+              class="tab-pane fade" 
+              :class="{ show: index === 0, active: index === 0 }" 
               role="tabpanel"
-              :aria-labelledby="`pills-day${index}-tab`"
+              :aria-labelledby="`pills-day${index}-tab`" 
               tabindex="0">
 
               <div class="event-widget-area">
+                <!-- Loop through events -->
                 <div class="row" v-for="(event, i) in day.events" :key="i" style="margin-bottom: 2rem;">
-                  <div class="col-lg-12">
-                    <ul class="event-widget-list">
-                      <li>
-                        <a href="#"><img src="/assets/img/icons/clock1.svg" alt="" />{{ event.time }} <span> | </span></a>
-                      </li>
-                    </ul>
-                    <h3>{{ event.title }}</h3>
-
-                    <!-- If sessions exist -->
-                    <ul v-if="event.sessions" class="mt-3" style="list-style: none; padding-left: 0;">
-                      <li v-for="(session, j) in event.sessions" :key="j" class="text-light">
-                        • {{ session }}
-                      </li>
-                    </ul>
-
+                  <div class="col-lg-10 m-auto">
+                    <div class="event2-boxarea box1 p-4">
+                      <div class="row align-items-center">
+                        <div class="col-lg-10">
+                          <div class="content-area">
+                            <h3>{{ event.title }}</h3>
+                          </div>
+                           <!-- Sessions block moved inside the event loop -->
+                      <div class="row" v-if="event.sessions?.length" style="margin-top: 2rem;">
+                        <div class="col-lg-10 m-auto">
+                          <div class="event2-boxarea box1 p-4">
+                            <div class="content-area">
+                              <h5>Sessions</h5>
+                              <ul 
+                                v-for="(session, j) in event.sessions" 
+                                :key="j" 
+                                class="mt-3" 
+                                style="list-style: none; padding-left: 0;">
+                                <li>• {{ session }}</li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- End sessions block -->
+                        </div>
+                        <div class="col-lg-2">
+                          <div class="img1">
+                            <img src="/assets/img/all-images/event/event-img22.png" style="height: 70px; width: 150px;" alt="" />
+                          </div>
+                        </div>
+                        <div class="space22"></div>
+                        <div class="col-lg-12">
+                          <div class="list">
+                            <ul>
+                              <li class="content-area">
+                                   <h5><img src="/assets/img/icons/clock1.svg" alt="" />&nbsp; {{ event.time }}</h5>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
+                <!-- End event loop -->
               </div>
-
             </div>
           </div>
         </div>
       </div>
+
     </div>
   </div>
 </template>
